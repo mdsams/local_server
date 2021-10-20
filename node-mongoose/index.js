f25d5cf07c84f56215715ded4cc7ac7952eb97a3
mdsams <tabrejmdsams786@gmail.com>
2021-10-20 23:51:11 +0530

    Mongoose Part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -16,10 +16,25 @@ connect.then((db) => {
     .then((dish) => {
         console.log(dish);
 
-        return Dishes.find({});
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'},
+        },{
+            new: true
+        }).exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feelings!',
+            author: 'Shams Tabrez'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
 
         return Dishes.remove({});
     })
